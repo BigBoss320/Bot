76d33b6933006163715e97a36561bd31caf9287c
Horziox <43693672+Horziox@users.noreply.github.com>
2018-10-14 14:22:21 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,1 +1,10 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
+var prefix = ("!");
+
+bot.login(process.env.TOKEN);
+
+bot.on('ready', fonction() {
+       console.log("Le Bot est en ligne !");
+};
