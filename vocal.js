75f89a3570ccc2b932d635a6117fc6a4a31b6ac4
BigBoss320 <44122706+BigBoss320@users.noreply.github.com>
2018-10-14 18:46:08 +0200

    Update vocal.js

--- vocal.js
+++ vocal.js
@@ -1,49 +1,14 @@
-Client.on('message', async message => {
-        if(message.content === prefix + "help") {
-            var aide_embed = new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setTitle(`:robot: Aide.`)
-        }
+if(message.member.voiceChannel)
+{
+    if(!message.guild.voiceConnection)
+    {
+        message.member.voiceChannel.join()
+            .then(connection >{
+                message.reply("Vocal rejoint avec succès !")
+            });
     }
-});
-
-
-
-
-client.on('message'), message => {
-    if(message.content[0] === prefix) {
-        let splitMessage = message.content.split(" ");
-        if(splitMessage[0] === prefix + 'play') {
-            if(splitMessage.length === 2)
-            {
-                if(message.member.voiceChannel)
-                {
-                    message.member.voiceChannel.join().then(connection => {
-                        dispatcher = connection.playArbitreryInput(splitMessage[1]);
-
-                        dispatcher.on('error', e => {
-                            dispatcher = undefined;
-                            console.log(e);
-                        });
-
-                        dispatcher.on('end', e => {
-                            console.log('Fin du son');
-                        })
-                    });
-                }
-                else
-                    sendError(message, 'Tu dois être dans un salon vocal pour utiliser cette commande !')
-            }
-            else
-                sendError(message, 'Erreur, problème dans les paramètres.');
-        }
-        else if(splitMessage[0] === prefix + 'pause') {
-           if(dispatcher !== undefined)
-            dispatcher.pause();
-        }
-        else if(splitMessage[0] === prefix + 'resume') {
-            if(dispatcher !== undefined)
-             dispatcher.resume();
-        }
-    }
-};
+}
+else
+{
+    message.reply("Tu dois être dans un vocal !");
+}
