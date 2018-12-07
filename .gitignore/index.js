d334f0d6eb10763c5787e7075a54f8eedd4e8154
Horziox <43693672+Horziox@users.noreply.github.com>
2018-12-07 17:39:44 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,9 +3,18 @@ const bot = new Discord.Client();
 const request = require('request');
 
 var prefix = ("!");
+const cleF = (process.env.cleF)
+
+const solo = require("./Commandes/Fortnite/solo.js");
+const duo = require("./Commandes/Fortnite/duo.js");
+const squad = require("./Commandes/Fortnite/squad.js");
+const lifetime = require("./Commandes/Fortnite/lifetime.js");
 
 bot.login(process.env.TOKEN);
 
 bot.on('ready', function() {
        console.log("Le Bot est en ligne !");
+});
+
+bot.on('message', message =>{
 });
